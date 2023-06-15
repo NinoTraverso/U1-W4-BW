a6b1ff959ce42410d06fc3f31d033a53a28fb8d0
NinoTraverso <njt98git@proton.me>
2023-06-15 18:40:39 +0000

    aggiustato layout

--- js/script_RESULTS.js
+++ js/script_RESULTS.js
@@ -91,18 +91,3 @@ const addTextToSvg = function (input) {
 };
 
 addTextToSvg(wrong);
-/*
-function resultPositiveNegative(percentualeTest) {
-  const div = document.createElement("div");
-  const paragrafo = document.createElement("p");
-
-  if (percentualeTest >= 60) {
-    paragrafo.innerText = "Hai superato il Test Sei Un Drago";
-  } else {
-    paragrafo.innerText = "Sei stato Bocciato Fai CACARE";
-  }
-
-  div.appendChild(paragrafo);
-  document.body.appendChild(div);
-}
-*/
